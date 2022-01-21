ee39c986eefc9bfed940003333ba517cb1d3207a
chrisoney <christopher.oney@yahoo.com>
2022-01-21 11:09:07 -0800

    testing simple route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hey there')
+})
 
 
 const port = 8080;
