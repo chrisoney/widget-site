fb2dc64ff12b375765b235cd8b54cb1bf7b4e38b
chrisoney <christopher.oney@yahoo.com>
2022-01-22 14:11:59 -0800

    start building out the index file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,34 @@
 import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import csurf from 'csurf';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+const  { environment } = require('./config/index.js');
 
 const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+const isProduction = environment === 'production';
+
+if (isProduction) app.use(cors());
+
+app.use(helmet({
+  contentSecurityPolicy: false
+}))
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "lax",
+      httpOnly: true,
+    }
+  })
+)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hey there')
