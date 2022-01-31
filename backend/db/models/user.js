5fdb7feb8650fdc5a8e30cc3ae4ad55d816bcf65
chrisoney <christopher.oney@yahoo.com>
2022-01-31 09:45:28 -0800

    add scopes to user model

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -35,7 +35,21 @@ module.exports = (sequelize, DataTypes) => {
         len: [60, 60]
       },
     },
-  }, {});
+  }, {
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'],
+      },
+    },
+    scopes: {
+      currentUser: {
+        attributes: { exclude: ['hashedPassword'] },
+      },
+      loginUser: {
+        attributes: {},
+      },
+    },
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
