b32fe1f848d6fe1cade1f8932b4934f04121e657
chrisoney <christopher.oney@yahoo.com>
2022-02-02 13:11:36 -0800

    start building out frontend

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware, compose, Store} from 'redux';
+import thunk from 'redux-thunk';
+
+let enhancer: any;
+
+interface windowWithRedux extends Window {
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: any;
+}
+
+if (process.env.NODE_ENV === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require('redux-logger').default;
+  const myWindow: windowWithRedux = window;
+  const composeEnhancers = myWindow.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const rootReducer = combineReducers({
+})
+
+const configureStore = (preloadedState: {} | undefined): Store => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
